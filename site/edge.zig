7e0ba08f1c892f203b159d517a7d2770d9f0f0c1
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-02-10 11:17:56 +0600

    fix: wasm todo page rename

--- site/edge.zig
+++ site/edge.zig
@@ -17,12 +17,12 @@ export fn main() void {
         .arena = allocator,
     };
 
-    const cmp = @import("pages/examples/wasm/page.zig").Page(ctx);
+    const cmp = @import("pages/examples/wasm/page-wa.zig").Page(ctx);
     var aw = std.Io.Writer.Allocating.init(allocator);
     defer aw.deinit();
     cmp.render(&aw.writer) catch {};
 
-    std.log.info("{s}", .{aw.written()});
+    // std.log.info("{s}", .{aw.written()});
 }
 
 pub const std_options = zx.std_options;
