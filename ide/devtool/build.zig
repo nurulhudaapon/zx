407c349125380dd5810d046368ad4a4d0005b1dc
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-02-14 21:57:00 +0600

    sync

--- ide/devtool/build.zig
+++ ide/devtool/build.zig
@@ -16,9 +16,8 @@ pub fn build(b: *std.Build) !void {
     const platform = b.option(Platform, "platform", "Platform to build for");
     build_options.addOption(Platform, "platform", platform orelse .development);
 
-    // --- ZX Setup (sets up ZX, dependencies, executables and `serve` step) ---
-    const site_exe = b.addExecutable(.{
-        .name = "zx_site",
+    const exe = b.addExecutable(.{
+        .name = "ziex_devtool",
         .root_module = b.createModule(.{
             .root_source_file = b.path("app/main.zig"),
             .target = target,
@@ -27,6 +26,6 @@ pub fn build(b: *std.Build) !void {
         }),
     });
 
-    site_exe.root_module.addOptions("build_options", build_options);
-    _ = try zx.init(b, site_exe, .{});
+    exe.root_module.addOptions("build_options", build_options);
+    _ = try zx.init(b, exe, .{});
 }
